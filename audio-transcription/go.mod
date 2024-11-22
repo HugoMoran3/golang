dc04de489bd9ea7572074da36b7de6a5278cf9eb
hugomoran3 <hugo.moran3@gmail.com>
2024-11-22 15:31:46 +1030

    Added http server

--- audio-transcription/go.mod
+++ audio-transcription/go.mod
@@ -1,3 +1,5 @@
 module audio-transcription
 
 go 1.23.2
+
+require github.com/sashabaranov/go-openai v1.35.7
--- audio-transcription/main.go
+++ audio-transcription/main.go
@@ -3,11 +3,21 @@ package main
 import (
 	"context"
 	"fmt"
+	"net/http"
 
 	openai "github.com/sashabaranov/go-openai"
 )
 
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w,"Hi there, I love to transcribe audio!", r.URL.Path[1:])
+}
+
 func main() {
+	http.HandleFunc("/", handler)
+	http.ListenAndServe(":8080", nil)
+}
+
+func transcribe() {
 	c := openai.NewClient("your token")
 	ctx := context.Background()
 
